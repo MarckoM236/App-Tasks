45e9bcf8ec240af9001e1bb33271de2bb43c3862
Marcko <marcko236@gmail.com>
2023-03-08 12:50:31 -0500

    Agregar archivos de proyecto.

---
+++ ToDo/ToDo/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ToDo.Data;
+using ToDo.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo
+{
+    public partial class App : Application
+    {
+        public static DatabaseContext Context { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+            MainPage = new NavigationPage(new HomePage());
+        }
+
+        private void InitializeDatabase()
+        {
+            var folderApp=Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var dbPath = System.IO.Path.Combine(folderApp, "ToDo3");
+            Context= new DatabaseContext(dbPath);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ToDo/ToDo/Views/HomePage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            LoadItems();
+        }
+
+        private async void LoadItems()
+        {
+            var items = await App.Context.GetItemsAsync();
+            lista_tareas.ItemsSource = items;
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddPage());
+        }
+
+        private async void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Confirmacion", "Realmente desea Eliminar esta tarea?", "Si", "No"))
+            {
+                var item = (ToDoItem)(sender as MenuItem).CommandParameter;
+                var result = await App.Context.DeleteItemAsync(item);
+                if (result == 1)
+                {
+                    LoadItems();
+                }
+            }
+        }
+    }
+}
